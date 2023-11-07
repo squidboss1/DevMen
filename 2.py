4b633a30a781d0f3673621e2307e196a68779310
Squidboss1 <xxx147ac@gmail.com>
2023-11-07 21:26:00 +0100

    first commit

---
+++ 2.py
@@ -0,0 +1,16 @@
+import json
+
+dictonary_1 = {"key1" : "value1", "key2" : "value2"}
+
+def reverse(**kwargs):
+    dictonary_2 = {}
+    for k, v in kwargs.items():
+        dictonary_2[v] = k
+
+    return dictonary_2
+
+result = reverse(**dictonary_1)
+
+with open("result.txt", "w", encoding="utf-8") as plik:
+    # plik.write(json.dumps(result))
+    # json.dump(result, plik)

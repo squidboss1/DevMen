ea8bbb055e23f91ec521a567fefb9119be15d7d4
squidboss1 <131413493+squidboss1@users.noreply.github.com>
2024-01-09 00:05:40 +0100

    Delete generators directory

--- generators/5.py
+++
@@ -1,8 +0,0 @@
-text = "The quick brown fox jumps over the lazy dog is an English-language pangram—a sentence that contains all of the letters of the English alphabet"
-
-words = text.split()
-print(words)
-
-length_of_words = [len(word) for word in words if word.lower() != "the"]
-
-print(length_of_words)
